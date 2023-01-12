59d900da661bec3a9bb139a06032ab284f2e0693
Chris <chris.p.bradford@gmail.com>
2023-01-12 13:36:51 -0600

    Complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
-const removeFromArray = function() {
+// const removeFromArray = function(originalArray,...removalElementsArray) {
+//     filteredArray = [];
+//     for (item of originalArray){
+//         for (element of removalElementsArray){
+//             if (item !=element)
+//             filteredArray.push(item);
+//         }
+//     }
+//     return filteredArray;
+// }
 
-};
+ const removeFromArray = function(array,...removalElementArray) {
+     filteredArray =  array.filter((element) => !removalElementArray.includes(element));
+     return filteredArray;
+ };
+
+
+// function checkEquality(element,removalElement){
+//     if (element === removalElement)return true;
+//     return false;
+// }
 
 // Do not edit below this line
 module.exports = removeFromArray;
