f62bc1e4f9cd7ca94bd5876b2634d130b1c422f0
Chris <chris.p.bradford@gmail.com>
2023-01-12 13:56:15 -0600

    Complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(beginningInteger,endingInteger) {
+    if (!Number.isInteger(beginningInteger) || !Number.isInteger(endingInteger)){
+        return 'ERROR';
+    }
+    else if (beginningInteger <0 || endingInteger <0){
+        return 'ERROR';
+    }
+    min = Math.min(beginningInteger,endingInteger);
+    max = Math.max(beginningInteger,endingInteger)
+    sum = min;
+    for (i = min+1;i<=max;i++){
+        sum +=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
