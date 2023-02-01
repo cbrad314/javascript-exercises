cf40b67feba0c75b82b3218cd52e5165f2e9da06
Chris <chris.p.bradford@gmail.com>
2023-02-01 10:26:44 -0600

    complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    const characters = Array.from(
+        a.toLowerCase().replace(/[^A-Za-z]/g,'').split('')
+        );
+    const reversedCharacters = Array.from(characters.reverse());
+    return characters.every((character,index) => {
+        // console.log(character);
+        // console.log(reversedCharacters[((reversedCharacters.length -1)-index)]);
+        return character === reversedCharacters[((reversedCharacters.length -1)-index)]})
 };
 
 // Do not edit below this line
