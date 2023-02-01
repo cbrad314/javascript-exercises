3a48ddc573139a39b924d7ebefa82a8c80bd399c
Chris <chris.p.bradford@gmail.com>
2023-02-01 09:34:05 -0600

    complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+	return sumArray.reduce((total,element) => total + element,0);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((total,element) => total * element)
+    
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a===0) return a+1;
+  else {
+    const factors = [];
+    for (let index = 0; index < a; index++) {
+      factors[index] = index+1;
+      
+    }
+    return factors.reduce((total,element) => total*element);
+  }
+
 };
 
 // Do not edit below this line
