931b3f36b87c43712d2196ec30d84a3a9c88b30b
Chris <chris.p.bradford@gmail.com>
2023-02-02 09:45:03 -0600

    complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson,person,i) => 
+    {
+    const personAge = !person.yearOfDeath ? new Date().getFullYear() - person.yearOfBirth : person.yearOfDeath - person.yearOfBirth;
+    const oldestPersonAge = !oldestPerson.yearOfDeath ? new Date().getFullYear() - oldestPerson.yearOfBirth : oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+    return (personAge >= oldestPersonAge) ? person: oldestPerson;
+    }
+    )
 };
 
 // Do not edit below this line
